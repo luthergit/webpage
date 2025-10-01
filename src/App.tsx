3de64f43963fdb911632fdbf1f1d56f6c14090e7
luthergit <luthereze@yahoo.co.uk>
2025-10-01 19:43:25 +0100

    added extra description

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
       <div>
         <h1>React Webpage</h1>
         <p>This is a React webpage for Chatbot pipeline</p>
+        <p>It's my webpage</p>
+        
       </div>
     </>
   )
